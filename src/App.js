27bba3846b04f50a104d3c478c21e83106ee6e02
Rashadtheone <rashad.hammonds0114@gmail.com>
2024-04-30 17:57:32 -0400

    displayed petcollection

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import {PetProfileCollection } from './ui-components';
+ 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <PetProfileCollection  />
+      </header>
+    </div>
+  );
+}
+
+export default App;
